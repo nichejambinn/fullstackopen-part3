c62b9dd2d1b598a4d3ce29c4d2ef32b9d3066176
nichejambinn <nichejambinn@gmail.com>
2022-05-15 20:52:24 -0400

    Exercise 3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -56,6 +56,32 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  return Math.floor(Math.random() * 10000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json(
+      {
+        error: 'name missing'
+      }
+    )
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
